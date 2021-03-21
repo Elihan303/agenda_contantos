e7b59c21b8fc1d77038800dad36281bb7cca2ac4
Elihan303 <elihanth@hotmail.com>
2021-03-20 20:33:41 -0400

    nuevo todo

--- src/App.js
+++ src/App.js
@@ -1,144 +1,55 @@
-
+import React from 'react';
 import './App.css';
+import contactos from './contactos.json'
 //Boostrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 //font awesome
 //import {FontAwesomenIcon} from '@fortawesome/react-fontawesome';
+
+
 //Importar componentes
-//import Formulario from './Componentes/Formulario'
+//import AddCliente from './Componentes/AddCliente'
+import Formulario from './Componentes/Formulario'
+
+
+console.log(contactos);
+class App extends React.Component {
+   
+    state={
+        contactos: contactos
+    }
+
+    AddContacto= (nombre,apellido,telefono,correo)=>{
+        const nuevoContacto ={
+            nombre: nombre,
+            apellido: apellido,
+            telefono:telefono,
+            correo: correo,
+            id: this.state.contactos.length
+        }
+        this.setState({
+            contactos: [...this.state.contactos, nuevoContacto]
+        })
+    }
 
 
 
-function App() {
+render() {
+  
   return (
-    <div className="App">
-
-      <header className="border bg-light">
-        <nav className="navbar navbar-expand-lg navbar-light  justify-content-between container ">
-          <h2>
-            <a className="logo">  AgendaMax  </a>
-          </h2>
-
-          <div className="header__nav">
-
-            <nav>
-              <a  className="header_nav_a" href="https://www.youtube.com/channel/UCj31CP1tmMZkf6jVgdfhIgQ">Youtube</a>
-              <a  className="header_nav_a" href="https://twitter.com/Jcod37954758">Twitter</a>
-              <a  className="header_nav_a" href="https://www.instagram.com/junior.eddn17/">Instagram</a>
-              <a  className="header_nav_a" href="https://github.com/Elihan303/agenda_contantos.git">Github</a>
-            </nav>
-
-          </div>
-
-
-          <div className="form-inline my-2 my-lg-0 flex-column">
-            <input className="form-control mr-sm-2" type="search" id='buscarCliente' placeholder="pulsar enter para buscar" aria-label="Search">   
-            </input>
-          </div>
-        </nav>
-      </header>
-
-      <div className="generic">
-
-        <div className="container clientesResultados">
-            <table className="table table-hover border " id='bodyDeTablaResult'>
-            </table>
-            <h2 id='searchResult'> </h2>
-        </div>
-        <form className=" mt-4">
-            <div className="container d-flex flex-wrap">
-                <div className="formulario col-12 col-md-4 mb-4">
-                    <div className="card">
-                        <div className="card-header">
-                            <h3>Contactos</h3>
-                        </div>
-                        <div className="card-body">
-                            <div className="form-group">
-                                <label>Nombre</label>
-                                <input type="text" className="form-control" id='nombre' name='nombre'
-                                    placeholder="nombre">
-                                </input>
-                                <i className="fas fa-check-circle"></i>
-                                <i className="fas fa-exclamation-circle"></i>
-                                <small>Error Message</small>
-                            </div>
-                            <div className="form-group">
-                                <label>Apellido</label>
-                                <input type="text" className="form-control" id='apellido' name='apellido'
-                                    placeholder="apellido">
-                                </input>
-                                <i className="fas fa-check-circle"></i>
-                                <i className="fas fa-exclamation-circle"></i>
-                                <small>Error Message</small>
-                            </div>
-
-                            <div className="form-group">
-                                <label >Numero de telefono</label>
-                                <input type="text" className="form-control" id='telefono' name='telefono'
-                                    placeholder="telefono">
-                                </input>
-                                <i className="fas fa-check-circle"></i>
-                                <i className="fas fa-exclamation-circle"></i>
-                                <small>Error Message</small>
-                            </div>
-
-
-
-                            <div className="form-group">
-
-                                <label>E-mail</label>
-                                <input type="email" className="form-control" placeholder="example@email.com" id="email">
-                                </input>
-                                <input className="fas fa-check-circle"></input>
-                                <i className="fas fa-exclamation-circle"></i>
-
-                                <small>Error Message</small>
-                            </div>
-          
-                        </div>
-                        <button className="btn btn-success" id='botonDeCrear'> Agregar</button>
-                    </div>
-                </div>
-                <div className="clientes col-12 col-md-8">
-                    <table className="table table-hover border">
-                        <thead>
-                            <tr>
-                                <th scope="col">nombre</th>
-                                <th scope="col">Apellido</th>
-                                <th scope="col">Telefono</th>
-                                <th scope="col">Correo</th>
-
-                                <th scope="col">configuracion</th>
-                            </tr>
-                        </thead>
-                        <tbody id='bodyDeTabla'>
-                            {/* AQUI MUESTRO LOS CLIENTES   */}
-
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </form>
-
-
-    </div>
-    <br/> 
-    <div className="footer">
-        <p className="footer_p">Copyright © 2010-2021
-            TeamPicapollo
-            Software</p>
-    </div>
-     
-
-  </div >
+      <div>
+      this
+   
+      <Formulario addContactos={this.addContacto} contactos={this.state.contactos} />
+      </div>
+ 
 
 
 
 
+  )
+}
 
-
-
-  );
 }
 
 
--- src/Componentes/Formulario.js
+++ src/Componentes/Formulario.js
@@ -1,78 +1,212 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Formulario extends React.Component {
-    render() {
-        
-    //     const AgregarContanto = ()=>{
-    //     let nombre = document.getElementById('input_nombre').value;
-    //      let apellido = document.getElementById('input_apellido').value;
-    //      let correo = document.getElementById('input_correo').value;
-    //      let telefono = document.getElementById('input_correo').value;
-    //      let contactos=  [nombre,apellido,correo,telefono]
-    //     let resultado= alert(contactos)
-    //    return resultado;
-        
-    //      }
-    //   document.getElementById('btn-enviar').addEventListener('click', AgregarContanto);
+export default class Formulario extends React.Component {
 
-    const f =()=>{
-        alert("Hola")
+
+    state = {
+        nombre: "",
+        apellido: "",
+        telefono: "",
+        correo: "",
+
     }
-        
 
+    CaptureData = (e) => {
+        console.log(e.target.name, e.target.value);
+        this.setState({
+            [e.target.name]: e.target.value
+
+        })
+
+
+    }
+    
+
+    insertarDatos = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        this.props.addContacto(this.state.nombre,this.setState.apellido,this.state.telefono,this.state.correo);
+    }
+
+    
+
+    render() {
 
         return (
-            <div className="Formulario">
-                <form>
-                    {/* Nombre */}
-                    <div className="">
-                        <label for="Nombre" className="formulario__label"><b>Nombre</b></label>
-                        <div class="formulario__grupo-input">
-                            <input id="input_nombre" type="text" className="formulario__input " placeholder="Nombre">
-                            </input>
-                        </div>
-                    </div>
-                    {/* Apellido */}
-                    <div className="">
-                        <label for="Apellido" className="formulario__label"><b>Apellido</b></label>
-                        <div class="formulario__grupo-input">
-                            <input id="input_apellido" type="text" className="formulario__input " placeholder="Apellido">
-                            </input>
-                        </div>
-                    </div>
-                    {/* Correo */}
-                    <div className="">
-                        <label for="Nombre" className="formulario__label"><b>Correo</b></label>
-                        <div class="formulario__grupo-input">
-                            <input id="input_correo" type="text" className="formulario__input " placeholder="you@email.com">
-                            </input>
-                        </div>
-                    </div>
-                    {/* Telefono */}
-                    <div className="">
-                        <label for="Nombre" className="formulario__label"><b>Telefono</b></label>
-                        <div class="formulario__grupo-input">
-                            <input id="input_telefono" type="text" className="formulario__input " placeholder="000-000-0000">
-                            </input>
-                        </div>
-                    </div>
+            <div className="App">
 
-                    <button onClick={f}  className="btn_enviar ">Enviar</button>
-                </form>
-            </div>
+                <header className="border bg-light">
+                    <nav className="navbar navbar-expand-lg navbar-light  justify-content-between container ">
+                        <h2>
+                            <a className="logo">  AgendaMax  </a>
+                        </h2>
 
-        );
+                        <div className="header__nav">
 
-       
+                            <nav>
+                                <a className="header_nav_a" href="https://www.youtube.com/channel/UCj31CP1tmMZkf6jVgdfhIgQ">Youtube</a>
+                                <a className="header_nav_a" href="https://twitter.com/Jcod37954758">Twitter</a>
+                                <a className="header_nav_a" href="https://www.instagram.com/junior.eddn17/">Instagram</a>
+                                <a className="header_nav_a" href="https://github.com/Elihan303/agenda_contantos.git">Github</a>
+                            </nav>
+
+                        </div>
+
+
+                        <div className="form-inline my-2 my-lg-0 flex-column">
+                            <input className="form-control mr-sm-2" type="search" id='buscarCliente' placeholder="pulsar enter para buscar" aria-label="Search">
+                            </input>
+                        </div>
+                    </nav>
+                </header>
+
+                <div className="generic">
+
+                    <div className="container clientesResultados">
+                        <table className="table table-hover border " id='bodyDeTablaResult'>
+                        </table>
+                        <h2 id='searchResult'> </h2>
+                    </div>
+                    <form className=" mt-4">
+                        <div className="container d-flex flex-wrap">
+                            <div className="formulario col-12 col-md-4 mb-4">
+                                <div className="card">
+                                    <div className="card-header">
+                                        <h3>Contactos</h3>
+                                    </div>
+                                    <div className="card-body">
+                                        <div className="form-group">
+                                            <label>Nombre</label>
+                                            <input type="text"
+                                                className="form-control"
+                                                id="nombre"
+                                                name="nombre"
+                                                placeholder="nombre"
+                                                onChange={this.CaptureData}
+                                            >
+                                            </input>
+                                            <i className="fas fa-check-circle"></i>
+                                            <i className="fas fa-exclamation-circle"></i>
+                                            <small>Error Message</small>
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Apellido</label>
+                                            <input type="text"
+                                                className="form-control"
+                                                id="apellido"
+                                                name="apellido"
+                                                placeholder="apellido"
+                                                onChange={this.CaptureData}
+                                            >
+
+                                            </input>
+                                            <i className="fas fa-check-circle"></i>
+                                            <i className="fas fa-exclamation-circle"></i>
+                                            <small>Error Message</small>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label >Numero de telefono</label>
+                                            <input type="text"
+                                                className="form-control"
+                                                id="telefono"
+                                                name="telefono"
+                                                placeholder="telefono"
+                                                onChange={this.CaptureData}
+                                            >
+                                            </input>
+                                            <i className="fas fa-check-circle"></i>
+                                            <i className="fas fa-exclamation-circle"></i>
+                                            <small>Error Message</small>
+                                        </div>
+
+
+
+                                        <div className="form-group">
+
+                                            <label>E-mail</label>
+                                            <input type="email"
+                                                className="form-control"
+                                                placeholder="example@email.com"
+                                                id="email"
+                                                name='correo'
+                                                onChange={this.CaptureData}
+                                            >
+                                            </input>
+                                            <input className="fas fa-check-circle"></input>
+                                            <i className="fas fa-exclamation-circle"></i>
+
+                                            <small>Error Message</small>
+                                        </div>
+
+                                    </div>
+                                    <button type="click" className="btn btn-success" id='botonDeCrear' onClick={this.insertarDatos} > Agregar</button>
+                                </div>
+                            </div>
+                            <div className="clientes col-12 col-md-8">
+                                <table className="table table-hover border">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Nombre</th>
+                                            <th scope="col">Apellido</th>
+                                            <th scope="col">Telefono</th>
+                                            <th scope="col">Correo</th>
+
+                                            <th scope="col">Configuracion</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id='bodyDeTabla' >
+                                        {this.props.contactos.map(e => 
+                                            <tr>
+                                            <th scope="row">{e.id}</th>
+                                            <th>
+                                                {e.nombre}
+                                            </th>
+                                            <th>
+                                                {e.apellido}
+                                            </th>
+                                            <th>
+                                                {e.telefono}
+                                            </th>
+                                            <th>
+                                                {e.correo}
+                                            </th>
+                                            <th>
+                                                <button className="btn btn-danger">Editar</button>
+                                                {"   "}
+                                                <button className="btn btn-success">Eliminar</button>
+                                            </th>
+                                        </tr>
+                                        )
+                                        }
+     )
+
+                                    </tbody>
+
+
+                                </table>
+                            </div>
+                        </div>
+                    </form>
+
+
+                </div>
+                <br />
+                <div className="footer">
+                    <p className="footer_p">Copyright © 2010-2021
+                    TeamPicapollo
+            Software</p>
+                </div>
+
+                {/* <AddCliente nombre={this.state.nombre} apellido={this.state.apellido} telefono={this.state.telefono} correo={this.state.correo}/> */}
+
+            </div >
+
+        )
+
     }
-
 
 
 }
-
-
-
-
-
-
-export default Formulario;
