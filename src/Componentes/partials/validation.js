06b415c5b6e0f4d3b0f97ca635fade491b61c547
willysjose2026 <50811468+willysjose2026@users.noreply.github.com>
2021-03-21 23:56:57 +0400

    Update validation.js

--- src/Componentes/partials/validation.js
+++ src/Componentes/partials/validation.js
@@ -5,4 +5,12 @@ function email_validacion (props){
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(props);
 }
 
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+    formControl.className = 'form-control is-invalid';
+    small.className = 'invalid-feedback';
+	small.innerText = message;
+}
+
 export default email_validacion;
